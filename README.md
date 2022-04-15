77291e105b5ad70cb71f6a0845a1858e0b79814b
Trevor Manz <trevor.j.manz@gmail.com>
2022-04-15 13:29:42 -0400

    feat: set minimal chromium args

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ node gosling-screenshot.js example.json example.jpeg # or .png/.webp
 icat example.jpeg
 ```
 
-![Generated Gosling visualization](https://user-images.githubusercontent.com/24403730/163467127-cb87a33c-2484-4f87-888f-76bd83ecb80f.jpeg)
+![Generated Gosling visualization](https://user-images.githubusercontent.com/24403730/163602155-96b48c3b-f9b7-440f-a26c-7ba6fd25782c.jpeg)
+
--- gosling-screenshot.js
+++ gosling-screenshot.js
@@ -36,6 +36,45 @@ function html(spec, {
 </html>`;
 }
 
+// https://www.bannerbear.com/blog/ways-to-speed-up-puppeteer-screenshots/
+let minimalArgs = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+];
+
 /**
  * @param {string} spec
  * @param {import("puppeteer").ScreenshotOptions} opts
@@ -52,7 +91,11 @@ async function screenshot(spec, opts) {
 	let tmpfile = path.join(__dirname, "tmp.html");
 	await fs.writeFile(tmpfile, html(spec));
 
-	let browser = await puppeteer.launch();
+	let browser = await puppeteer.launch({
+		headless: true,
+		args: minimalArgs,
+		userDataDir: path.join(__dirname, ".cache"),
+	});
 
 	let page = await browser.newPage();
 	await page.goto(`file://${tmpfile}`, { waitUntil: "networkidle0" });
